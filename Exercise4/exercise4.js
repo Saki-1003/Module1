f109d37cfdcd41b26cb755938c976ea5e59f167b
Saki-1003 <saki.a19851003@gmail.com>
2024-05-01 16:54:35 +1200

    New branch for extra exercise

---
+++ Exercise4/exercise4.js
@@ -0,0 +1,26 @@
+function addition(a, b){
+  return a + b
+}
+
+console.log(addition(12, 15))
+
+function subtraction(a, b){
+  return a - b
+}
+console.log(subtraction(12, 15))
+
+function multiply(a, b){
+  return a * b
+}
+console.log(multiply(12, 15))
+
+function divide(a, b){
+  return a / b
+}
+console.log(divide(12, 15))
+
+function person(name){
+  console.log('Hello ' + name)
+}
+
+person('Saki')
